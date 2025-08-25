d719d4f6dcd9e81389ce33cb4eb9f757f7a922a3
Anhad401 <akohli_be24@thapar.edu>
2025-08-25 09:39:54 +0530

    message

--- dsalabs.cpp
+++ dsalabs.cpp
@@ -354,19 +354,25 @@ using namespace std;
 //     cout<<"after concatenation: "<<s1<<endl;
 
 // (b) Reverse
-//     char str1[100];
-//     cout<<"\n(b) enter string to reverse: ";
-//     cin>>str1;
-//     int n=strlen(str1);
-//     cout<<"reverse: ";
-//     for(int k=n-1;k>=0;k--){
-//         cout<<str1[k];
-//     }
-//     cout<<endl;
+//     int main() {
+//     char str[100];
+//     cout << "Enter a string: ";
+//     cin >> str;
 
+//     int n = strlen(str);
+
+//     for (int i = 0; i < n/2; i++) {
+//         char temp = str[i];
+//         str[i] = str[n-i-1];
+//         str[n-i-1] = temp;
+//     }
+
+//     cout << "Reversed string: " << str << endl;
+//     return 0;
+// }
 //   (c) Delete vowels
 //     char str2[100];
-//     cout<<"\n(c) enter string to delete vowels: ";
+//     cout<<"\n enter string to delete vowels: ";
 //     cin>>str2;
 //     int p=0,q=0;
 //     while(str2[p]!='\0'){
